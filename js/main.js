2edffe970e8784bf5d1dc3716a2c8dc08df4c751
Walltaz89 <194965379+Walltaz89@users.noreply.github.com>
2025-03-01 21:23:22 -0600

    css stuff plus more

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-// JS scripts placed here
+const hamburger = document.querySelector('.hamburger');
+const menu = document.getElementById('main-menu');
+
+hamburger.addEventListener('click', () => {
+  const expanded = hamburger.getAttribute('aria-expanded') === 'true' || false;
+  hamburger.setAttribute('aria-expanded',!expanded);
+  menu.classList.toggle('expanded');
+});
+
